02cd549beb6a3ca9fc5971931bd397a82936da9f
saesacga <139521874+saesacga@users.noreply.github.com>
2023-07-16 23:23:46 -0500

    Quedarse pegado en las paredes agregado
    Para facilitar el salto entre paredes se añadió la mecánica de quedarse pegado un par de segundos para recalcular el salto.

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Members
+
     public CharacterController controller;
     private Vector3 move;
 
@@ -15,12 +17,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _dashSpeed;
     [SerializeField] private float _dashTime;
     [SerializeField] private float _dashCooldown;
+    private bool _inDash = false;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    Vector3 velocity;
+    private Vector3 velocity;
+    
     bool isGrounded;
     private bool inTheAir = false;
 
@@ -30,33 +34,56 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 myInput;
     [SerializeField] private float smoothInputSpeed;
 
-    private bool wallJumpFlag;
+    private bool wallJumpFlag = false;
     private bool wallJump = true;
 
-    private float _contador = 0;
+    private float _dashContador = 0;
+    private float _wallJumpContador = 0;
+
+    #endregion
+    
     void Update()
     {
-        //Jump
+        #region Wall Jump
+        
+        switch (_wallJumpContador)
+        {
+            case <= 3:
+                _wallJumpContador += Time.deltaTime;
+                break;
+            case >= 3:
+                wallJumpFlag = false;
+                break;
+        }
         if (wallJumpFlag == true && wallJump == true && inTheAir == true && ActionMapReference.playerMap.Movimiento.Jumping.WasPerformedThisFrame())
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            wallJump = false;
+            WallJump();
         }
+        
+        #endregion
 
-        if (ActionMapReference.playerMap.Movimiento.Jumping.WasPerformedThisFrame() == true && isGrounded)
+        #region Jump
+        
+        else if (ActionMapReference.playerMap.Movimiento.Jumping.WasPerformedThisFrame() == true && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             speed -= airSpeedReduction;
             inTheAir = true;
         }
-
+        
+        #endregion
+        
         #region Dash
-        if (_contador <= _dashCooldown) { _contador += Time.deltaTime; }
-        if (ActionMapReference.playerMap.MovimientoAvanzado.Dash.WasPerformedThisFrame() && _contador >= _dashCooldown)
+        
+        if (_dashContador <= _dashCooldown) { _dashContador += Time.deltaTime; }
+        if (ActionMapReference.playerMap.MovimientoAvanzado.Dash.WasPerformedThisFrame() && _dashContador >= _dashCooldown)
         {
             StartCoroutine(Dash());
         }
+        
         #endregion
+
+        #region GroundCheck
         
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if (isGrounded && velocity.y < 0)
@@ -68,19 +95,25 @@ public class PlayerMovement : MonoBehaviour
                 inTheAir = false;
             }
         }
-
+        
+        #endregion
+        
         #region Movimiento
+        
         myInput.x = ActionMapReference.playerMap.Movimiento.Move.ReadValue<Vector2>().x;
         myInput.z = ActionMapReference.playerMap.Movimiento.Move.ReadValue<Vector2>().y;
 
         currentInputVector = Vector3.SmoothDamp(currentInputVector, myInput, ref smoothInputVelocity, smoothInputSpeed);
         move = transform.right * currentInputVector.x + transform.forward * currentInputVector.z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (wallJumpFlag == false)
+        {
+            controller.Move(move * speed * Time.deltaTime); //Movimiento horizontal (Caminar)
+            controller.Move(velocity * Time.deltaTime); //Movimiento vertical (Salto)
+        }
 
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        if (wallJumpFlag == false && _inDash == false) { velocity.y += gravity * Time.deltaTime; }
+        
         #endregion
     }
 
@@ -88,10 +121,13 @@ public class PlayerMovement : MonoBehaviour
     {
         float startTime = Time.time;
         
-        while (Time.time < startTime + _dashTime && move != new Vector3(0f,0f,0f))
+        while (Time.time <= startTime + _dashTime && move != new Vector3(0f,0f,0f))
         {
+            if (Time.time >= startTime + _dashTime) { _inDash = false; }
+            else { _inDash = true; }
+            
             controller.Move(move * _dashSpeed * Time.deltaTime);
-            _contador = 0;
+            _dashContador = 0;
 
             #region Raycast
             Ray ray = GetComponentInChildren<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -112,13 +148,26 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void WallJump()
     {
-        if (collision.collider.CompareTag("WallJump")) { wallJumpFlag = true; }
+        wallJumpFlag = false;
+        wallJump = false;
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("WallJump") && inTheAir)
+        {
+            _wallJumpContador = 0;
+            wallJumpFlag = true;
+            velocity.y = 0f;
+        }
         else { wallJumpFlag = false; }
     }
     private void OnCollisionExit(Collision collision)
     {
         wallJump = true;
+        wallJumpFlag = false;
     }
 }
