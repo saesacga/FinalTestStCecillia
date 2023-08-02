623f148deb298df48aaba60b990270957466393d
saesacga <139521874+saesacga@users.noreply.github.com>
2023-08-02 01:22:03 -0500

    Finalizo por hoy
    No queda mucho tiempo ayuda

--- Assets/Scripts/Magic/Constellations.cs
+++ Assets/Scripts/Magic/Constellations.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Fungus;
 using UnityEngine;
 
 public class Constellations : MonoBehaviour
 {
     public int _starsRequired;
+    [SerializeField] private Flowchart _flowchart;
 
     [HideInInspector] public int _starsCount;
     [HideInInspector] public int _starsInPosition;
@@ -16,6 +18,7 @@ public class Constellations : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().enabled = true;
             GetComponent<Animator>().SetBool("startDancing", true);
+            _flowchart.ExecuteBlock("DeafultBlock");
         }
     }
 }
