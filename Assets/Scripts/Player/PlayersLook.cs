afd7d875659d383ba624b473d54f67a60fc74681
saesacga <139521874+saesacga@users.noreply.github.com>
2024-02-23 17:39:49 -0500

    LA CÁMARA FUNCIONA POR FIN
    WUUUUUUU!!!!!!

--- Assets/Scripts/Player/PlayersLook.cs
+++ Assets/Scripts/Player/PlayersLook.cs
@@ -23,7 +23,7 @@ public class PlayersLook : MonoBehaviour
     {
         mouseSensitivityAimAssist = mouseSensitivity;
     }
-
+    
     void Update()
     {
         myMouseInput.x = ActionMapReference.playerMap.Movimiento.Look.ReadValue<Vector2>().x * mouseSensitivityAimAssist * Time.deltaTime;
@@ -36,16 +36,14 @@ public class PlayersLook : MonoBehaviour
             xRotation -= currentInputMouseVector.y;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
             
-            transform.Rotate(Vector3.left * currentInputMouseVector.y); //Referencia para la cámara arriba y abajo
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //Referencia para la cámara arriba y abajo
             _playerBody.Rotate(Vector3.up * currentInputMouseVector.x); //Mover jugador de izquierda a derecha
         }
         else //Igualar la rotación del POV con los movimientos de cámara
         {
-            
             transform.localRotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, 0f, 0f);
-            
+            xRotation = UnityEditor.TransformUtils.GetInspectorRotation(transform).x;
             _playerBody.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
-            
         }
     }
 }
