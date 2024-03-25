d43f54a37a0f79852c3b45e695eff4d578bc8db7
saesacga <139521874+saesacga@users.noreply.github.com>
2024-03-25 02:51:06 -0500

    Efectos añadidos
    Se cambió el rayo de luz por un orbe, el objetivo es que incite la exploración

--- Assets/Scripts/VisualEffects/SpriteBillboard.cs
+++ Assets/Scripts/VisualEffects/SpriteBillboard.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class SpriteBillboard : MonoBehaviour
 {
     [SerializeField] private bool activateBillboard;
+    [SerializeField] private bool _activateVerticalBillboard;
     void LateUpdate()
     {
         if (activateBillboard)
         {
-            transform.LookAt(Camera.main.transform.position, -Vector3.up);
-            transform.localEulerAngles = new Vector3(0,transform.localEulerAngles.y,0);
-            
-            //transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+            if (_activateVerticalBillboard)
+            {
+                transform.rotation = Camera.main.transform.rotation;
+            }
+            else
+            {
+                transform.LookAt(Camera.main.transform.position, -Vector3.up);
+                transform.localEulerAngles = new Vector3(0,transform.localEulerAngles.y,0);
+            }
         }
     }
 }
